dfdff2d4fe74eac3ae676d53e8072abf8ba3e907
Githubpoozas <po.oz.as@hotmail.com>
2020-07-15 00:15:18 +0700

    add score,finish good and evil gauge

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navigation">
-    <v-app-bar fixed app dark :style="navColor">
+    <v-app-bar fixed app dark :style="navColor()">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
 
       <v-toolbar-title v-text="navTitle" />
@@ -26,8 +26,21 @@
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title>Application</v-list-item-title>
-            <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+            <v-list-item-title>The Good one</v-list-item-title>
+            <v-list-item-subtitle>some guy</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content class="credit-box">
+            <p class="credit-text">
+              <span>Credit :</span>{{ credit.toLocaleString() }}
+            </p>
+            <p class="credit-text">
+              <span>Good :</span> {{ goodPoint.toLocaleString() }}
+            </p>
+            <p class="credit-text">
+              <span>Evil :</span> {{ evilPoint.toLocaleString() }}
+            </p>
           </v-list-item-content>
         </v-list-item>
 
@@ -58,9 +71,9 @@ export default {
   data() {
     return {
       drawer: false,
-      goodPoint: 50,
-      navColor:
-        "background: linear-gradient(90deg, rgba(33,150,243,1) 0%, rgba(213,0,0,1) 100%)",
+      goodPoint: 1,
+      evilPoint: 1,
+      credit: 10000,
       items: [
         {
           icon: "mdi-home",
@@ -103,6 +116,19 @@ export default {
       this.$store.dispatch("switchAction");
       if (this.isEvil) return this.$router.push("/evil");
       return this.$router.push("/good");
+    },
+    navColor() {
+      if (this.goodPoint === 0 || this.evilPoint === 0) return false;
+
+      let totalPoint = this.goodPoint + this.evilPoint;
+      let goodPercent = Math.floor((this.goodPoint / totalPoint) * 100);
+      return (
+        "background: linear-gradient(90deg, rgba(33,150,243,1) " +
+        (goodPercent - 10) +
+        "%, rgba(213,0,0,1) " +
+        (goodPercent + 10) +
+        "%)"
+      );
     }
   },
   watch: {
@@ -110,11 +136,10 @@ export default {
       this.isEvil ? this.goDark() : this.goLight();
     },
     goodPoint() {
-      this.navColor =
-        "background: linear-gradient(90deg, rgba(33,150,243,1) " +
-        this.goodPoint +
-        "%, rgba(213,0,0,1) 100%)";
-      console.log(this.navColor);
+      this.navColor();
+    },
+    evilPoint() {
+      this.navColor();
     }
   },
   computed: {
@@ -148,5 +173,13 @@ export default {
 .bg-noMan {
   background-image: url("~assets/background/bg-noMan.jpg");
   background-size: cover;
+}
+.credit-box .credit-text {
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+}
+.credit-box .credit-text:not(:last-child) {
+  margin-bottom: 5px;
 }
 </style>
